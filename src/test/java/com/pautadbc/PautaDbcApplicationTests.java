7f7f4a1d1e3511b09ae3ae3a8c2ad1d4274c1e7e
Michel Bernardo <michel_13s@outlook.com>
2023-12-25 19:45:12 -0300

    feat: adicioando pasta de teste e .jar

---
+++ src/test/java/com/pautadbc/PautaDbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pautadbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PautaDbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
