8412298904fb6d40c5aa1ccfe8e43e8c51a31695
Michel Bernardo <michel_13s@outlook.com>
2023-12-25 19:44:01 -0300

    feat: criando validador de cpf

---
+++ src/main/java/com/pautadbc/validador/Validador.java
@@ -0,0 +1,35 @@
+package com.pautadbc.validador;
+
+public class Validador {
+    public static boolean validarCPF(String cpf) {
+        cpf = cpf.replaceAll("[^0-9]", "");
+
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        if (cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += (cpf.charAt(i) - '0') * (10 - i);
+        }
+        int primeiroDigito = 11 - (soma % 11);
+        if (primeiroDigito > 9) {
+            primeiroDigito = 0;
+        }
+
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += (cpf.charAt(i) - '0') * (11 - i);
+        }
+        int segundoDigito = 11 - (soma % 11);
+        if (segundoDigito > 9) {
+            segundoDigito = 0;
+        }
+
+        return primeiroDigito == (cpf.charAt(9) - '0') && segundoDigito == (cpf.charAt(10) - '0');
+    }
+}
