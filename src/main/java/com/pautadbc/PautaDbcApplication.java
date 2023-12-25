471aa31f8e82adcad46c4a002c69c44cc4814a2d
Michel Bernardo <michel_13s@outlook.com>
2023-12-25 19:38:09 -0300

    feat: starting project

---
+++ src/main/java/com/pautadbc/PautaDbcApplication.java
@@ -0,0 +1,14 @@
+package com.pautadbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class PautaDbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PautaDbcApplication.class, args);
+	}
+
+}
